76177603851baee695c5559449200103d254e959
Grangy <grangy@live.ru>
2025-10-24 17:21:16 +0300

    fix: copy dist files to root directory to bypass Render path resolution issues

---
+++ config/env.d.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+export declare const env: {
+    DATABASE_URL: string;
+    BOT_TOKEN: string;
+    NODE_ENV: "development" | "test" | "production";
+    LOG_LEVEL: "error" | "fatal" | "warn" | "info" | "debug" | "trace";
+    PORT?: string | undefined;
+};
+//# sourceMappingURL=env.d.ts.map
